652c48d23dc06c01688b7150129d7112123c563a
xiaoxian <1172242099@qq.com>
2021-12-24 15:35:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||圣|诞|节|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
